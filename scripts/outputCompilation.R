5774c2b8f143c853438bb4d6cc6fd68ee5caf8b9
dcyr <cyr.dominic@gmail.com>
2019-02-21 11:25:12 -0500

    function for compiling fluxes added

--- scripts/outputCompilation.R
+++ scripts/outputCompilation.R
@@ -9,7 +9,6 @@ setwd(wwd)
 
 ###################################################################
 ###################################################################
-#require(raster)
 require(doSNOW)
 ###################################################################
 require(dbplyr)
@@ -20,7 +19,6 @@ require(RSQLite)
 ## https://datacarpentry.org/R-ecology-lesson/05-r-and-databases.html
 # require(dplyr)
 # require(tidyr)
-
 
 ### see Kurz et al 2009 - Table 2
 ### GPG: Good Practice Guidance
@@ -35,6 +33,171 @@ fert_AT <- read.csv("../classifiersAT/fert_AT.csv")
 dens_AT <- read.csv("../classifiersAT/dens_AT.csv")
 ct_AT <- read.csv("../classifiersAT/ct_AT.csv")
 
+
+###################################################################
+###################################################################
+### some useful functions
+###################################################################
+
+#### a function that select the appropriate pools for a given indicator    
+indicatorPoolSelect <- function(x) {## where 'x' is the name of the Indicator
+    ## example: Total Ecosystem
+    
+    ### identifying a subset of pools
+    poolIndex <- r_pool_indicators %>%
+        filter(name == x) %>%
+        merge(r_pool_collections,
+              by.x = "pool_collection_id",
+              by.y = "id") %>%
+        merge(r_pool_collection_pools) %>%
+        mutate(pool_indicator_id = id) %>%
+        select(pool_indicator_id,
+               name,
+               pool_collection_id,
+               description,
+               pool_id)
+    
+    ## formatting outputs into a clean list
+    x <- as.list(distinct(poolIndex[,1:4]))
+    x[["pool_id"]] <- poolIndex$pool_id
+    
+    return(x)
+}
+
+## A function that filters pools of interest and summarize by location
+## and gather relevant information
+poolSummarise <- function(x,
+                          groupingVar = c(
+                              dateDim = T,
+                              classifierSetDim = F,
+                              landClassDim = F,
+                              ageClassDim = F)
+) {
+    ## where x is the name of the Indicator
+    ## and where other variable indicate
+    ## whether or not we should use these
+    ## variable for grouping
+    ## extracting indicator info
+    indInfo <- indicatorPoolSelect(x)
+    
+    ## filtering for pools of interest and summarizing by location
+    x <- Pools %>%
+        filter(poolId %in% indInfo$pool_id) %>%
+        ## summaryizing by location (classifier and time steps, and age class?)
+        group_by(locationDimId) %>%
+        summarise(indicatorValue = sum(poolValue))
+    
+    x <- x %>%
+        merge(LocationDimension,
+              by.x = "locationDimId",
+              by.y = "id")
+    if (groupingVar["dateDim"]) {
+        x <- x %>%
+            merge(DateDimension[,c("id", "step", "year", "lengthOfStepInYears")],
+                  by.x = "dateDimId",
+                  by.y = "id") 
+    }
+    if (groupingVar["classifierSetDim"]) {
+        x <- x %>%
+            merge(ClassifierSetDimension,
+                  by.x = "classifierSetDimId",
+                  by.y = "id")
+    }
+    
+    ## summarizing using selected grouping variables
+    grVar <- names(groupingVar)[groupingVar]
+    grVar <- paste0(grVar, "Id")
+    ##
+    x <- x %>%
+        group_by(.dots=grVar) %>%
+        summarize(totalArea = sum(area),
+                  totalC = sum(indicatorValue))
+    
+    ### tidying up
+    cNames <- colnames(x)
+    
+    if ("landClassDimId" %in% cNames) {
+        x <- x %>%
+            merge(LandClassDimension,
+                  by.x = "landClassDimId",
+                  by.y = "id") %>%
+            arrange(name)
+    }
+    if ("ageClassDimId" %in% cNames) {
+        x <- x %>%
+            merge(AgeClassDimension,
+                  by.x = "ageClassDimId",
+                  by.y = "id") %>%
+            arrange(startAge)
+    }
+    if ("classifierSetDimId" %in% cNames) {
+        x <- x %>%
+            merge(ClassifierSetDimension,
+                  by.x = "classifierSetDimId",
+                  by.y = "id") %>%
+            arrange(coverType, relDensity, fertility)
+    }
+    if("dateDimId" %in% cNames) {
+        x <- x %>%
+            merge(DateDimension[,c("id", "step", "year", "lengthOfStepInYears")],
+                  by.x = "dateDimId",
+                  by.y = "id") %>%
+            arrange(year)
+    }
+    
+    x <- data.frame(as.data.frame(indInfo[1:4]),
+                    x)
+    
+    return(x)
+}
+
+## A function that filters fluxes of interest and summarize by location
+## and gather relevant information
+
+fluxSummarise <- function(x, ## where x is a r_stock_changes
+                          groupingVar = c(
+                              dateDim = T,
+                              classifierSetDim = F,
+                              landClassDim = F,
+                              ageClassDim = F)
+) {
+    ## where x is the name of the Indicator
+    ## and where other variable indicate
+    ## whether or not we should use these
+    ## variable for grouping
+    ## extracting indicator info
+
+    x <- v_stock_change_indicators %>%
+        filter(indicator == x)
+
+    grVarExcl <- c("area", "flux_tc")
+
+    if (!groupingVar[["ageClassDim"]]) {
+        grVarExcl <- append(grVarExcl, "age_range")
+    }
+    if (!groupingVar[["landClassDim"]]) {
+        grVarExcl <- append(grVarExcl, "unfccc_land_class")
+    }
+    if (!groupingVar[["classifierSetDim"]]) {
+        grVarExcl <- append(grVarExcl, c(colnames(ClassifierSetDimension)[-1]))
+    }
+    if (!groupingVar[["dateDim"]]) {
+        grVarExcl <- append(grVarExcl, "year")
+    }
+
+
+    colIndex <- which(colnames(x) %in% grVarExcl == F)
+
+    x <- x %>%
+        group_by(.dots = colnames(x)[colIndex]) %>%
+        summarize(area = sum(area),
+                  flux_tc = sum(flux_tc))
+    return(x)
+}
+
+foo <- fluxSummarise("Total Harvest (Biomass + Snags)")
+plot(foo$flux_tc)
+
 ###################################################################
 ###################################################################
 #### connect to db
@@ -48,7 +211,6 @@ tableNames <- dbListTables(gcbmOutputDB)
 
 ###################################################################
 #### load all tables (might need to prune a bit...)
-
 for (i in seq_along(tableNames)) {
     x <- tableNames[i]
     assign(x, dbReadTable(gcbmOutputDB, x))
@@ -57,238 +219,44 @@ for (i in seq_along(tableNames)) {
 dbDisconnect(gcbmOutputDB)
 
 
+# ###################################################################
+# #### create summarized data.frame with pools,
+# #### by time step, cover type, fertility class, and density class
+# 
+# poolSummary <- list()
+# for (i in r_pool_indicators$id) {
+#     
+#     index <- which(r_pool_indicators$id == i)
+#     x <- r_pool_indicators[index,"name"]
+#     print(paste("summarizing pools for", x))
+#     poolSummary[[i]] <- poolSummarise(x,
+#                                       groupingVar = c(
+#                                           dateDim = T,
+#                                           classifierSetDim = F,
+#                                           landClassDim = F,
+#                                           ageClassDim = F))
+#     print(paste("Done!"))
+# }
+# 
+# poolSummary <- do.call("rbind", poolSummary)
+# 
+# 
+# # ### quick plot
+# require(ggplot2)
+# 
+# options(scipen=999)
+# df <- filter(poolSummary, year >=2016)
+# 
+# 
+# head(df)
+# ggplot(df, aes(x = year, y = totalC/totalArea, group = name, color  = name)) +
+#     geom_line()
 
 
 
 
-###################################################################
-#### create summarized data.frame with pools,
-#### by time step, cover type, fertility class, and density class
+require(raster)
+r <- raster("../processed_output/spatial/AG_Biomass_C_2016.tiff")
 
-#### should possibly store info about last disturbances
-
-
-foo <- list()
-for (i in r_pool_indicators$id) {
-    
-    ### identifying a subset of pools
-    poolIndex <- r_pool_indicators %>%
-        filter(id == i) %>%
-        merge(r_pool_collections,
-              by.x = "pool_collection_id",
-              by.y = "id") %>%
-        merge(r_pool_collection_pools) %>%
-        mutate(pool_indicator_id = id) %>%
-        select(pool_indicator_id,
-               name,
-               pool_collection_id,
-               description,
-               pool_id)
-    
-    ### selecting a subset of pools
-    x <-  filter(Pools, poolId %in% poolIndex$pool_id)
-    
-    ### summarizing the indicator.... by classifier and time steps
-    ### (and age class?) ### see what happen with anthropogenic class
-    
-    DateDimension
-    LocationDimension
-    ClassifierSetDimension
-    
-    ### give proper names
-    
-    
-    ##########################
-    ### do the same thing for fluxes
-    r_flux_indicators
-    r_flux_indicator_collections
-    r_flux_indicator_collection_flux_indicators
-    
-
-    foo <- merge(Pools, poolIndex)
-    
-    ,
-                 by.x)
-    
-    
-    head(Pools)
-     %>%
-        merge(r_pool_collections)
-    r_pool_collections
-    poolID_subset <- 
-    
-    x <- Pools %>%
-        
-    
-    PoolDimension
-    r_pool_collections
-    r_pool_collection_pools
-    
-}
-
-
-foo <- Pools %>%
-    
-    
-    poolCol_GPG_subset <- c("Aboveground Biomass",
-                                 "Belowground Biomass",
-                                 "Deadwood",
-                                 "Litter",
-                                 "Soil Carbon")
-
-pool_indicator_subset <- match(poolCol_GPG_subset)
-
-r_pool_collections
-r_pool_collection_pools
-r_pool_indicators
-
-
-poolId_Subset <- 
-
-poolsSubset <- 
-
-    poolId
-
-foo <- Pools %>%
-    merge(filter )
-r_pool_collections_subset
-r_pool_collection_pools
-
-                 
-                 
-                 
-                 
-                         
-                         "HardwoodCoarseRoots",
-                         "HardwoodCoarseRoots",
-                         )
-                         "HardwoodMerch",
-                         "HardwoodMerch",
-                         "HardwoodMerch",
-                         "HardwoodMerch",
-                         
-                         
-                         
-                               )
-    DOM = c("AboveGroundFastSoil",
-                               "AboveGroundSlowSoil",
-                               "AboveGroundVeryFastSoil",
-                               "BelowGroundFastSoil",
-                               "BelowGroundSlowSoil",
-                               "BelowGroundVeryFastSoil",
-                               "DissolvedOrganicCarbon",
-                               
-                               
-                               
-                    "HardwoodBranchSnag",
-                    "AboveGroundSlowSoil",
-                    "AboveGroundSlowSoil",
-                    "AboveGroundSlowSoil",
-                    
-                    
-                    
-
-2   2     AboveGroundSlowSoil
-3   3 AboveGroundVeryFastSoil
-
-quantile(Pools$poolValue, c(0, 0.00001, 0.001, 0.01, 0.02,
-                  0.95, 0.99, 0.999, 0.99999, 1))
-
-
-#### Dimension tables
-PoolDimension <- as.data.frame(tbl(compiledDB, "PoolDimension"))
-LocationDimension <- as.data.frame(tbl(compiledDB, "LocationDimension")) # contains area (in ha)
-LandClassDimension <- as.data.frame(tbl(compiledDB, "LandClassDimension")) ## ex. "UNFCCC_FL_R_FL"
-ClassifierSetDimension <- as.data.frame(tbl(compiledDB, "ClassifierSetDimension"))
-ModuleInfoDimension <- as.data.frame(tbl(compiledDB, "ClassifierSetDimension"))
-moduleInfoDimId
-
-
-
-
-
-poolsEcosystem <- 
-
-#### Pool values 
-Pools <- as.data.frame(tbl(compiledDB, "Pools"))
-Fluxes <- as.data.frame(tbl(compiledDB, "Fluxes"))
-LandClassDimension
-
-as.data.frame(PoolDimension)
-df <- ClassifierSetDimension 
-head(df)
-unique(df$classifierSetDimId)
-
-
-summary(as.data.frame(pools))
-
-pool1 <- pools %>%
-    filter(poolID == 1) %>%
-    select(id, locationDimId, poolValue)
-
-#######
-
-
-
-outputFiles <- list.files(paste(simDir, "processed_output/spatial", sep ="/"))
-
-###################################################################
-###################################################################
-### visualizing pools 
-pools <- c("AG_Biomass_C",
-           "Dead_Organic_Matter_C",
-           "Soil_C",
-           "Total_Biomass_C",
-           "Total_Ecosystem_C")
-###################################################################
-
-nClusters <- min(10, length(pools)) ## 10 treads max with 32Gb of RAM
-cl <- makeCluster(nClusters, outfile = "")
-registerDoSNOW(cl)
-
-x <- foreach(i = seq_along(pools), .combine = "rbind")  %dopar% {#
-    require(raster)
-    
-    v <- pools[i]
-    
-    out <- paste(simDir, "processed_output/spatial",
-               outputFiles[grep(v, outputFiles)], sep = "/")
-
-    ## stacking rasters
-    r <- stack(out)
-    ## computing averages
-    out <- apply(values(r), 2, mean, na.rm = T)
-    lNames <- names(out)
-    
-    out <- data.frame(variable = v,
-                      year = as.numeric(substr(lNames,
-                                               nchar(lNames)-3, nchar(lNames))),
-                      value = out)
-    
-    return(out)
-}
-stopCluster(cl)
-
-### summarizing and tidying up
-df <- x %>%
-    spread(variable, value) %>%
-    mutate(BG_Biomass_C = Total_Biomass_C - AG_Biomass_C,
-           test =  AG_Biomass_C + BG_Biomass_C + Dead_Organic_Matter_C) %>%
-    select(year, AG_Biomass_C, BG_Biomass_C, Dead_Organic_Matter_C) %>%
-    gather(key = variable, value = value,
-           AG_Biomass_C, BG_Biomass_C, Dead_Organic_Matter_C)
-
-df$variable <- factor(df$variable,
-                      levels = c("AG_Biomass_C", "BG_Biomass_C", "Dead_Organic_Matter_C"))
-
-
-### plotting 
-require(ggplot2)
-
-p <- ggplot(df, aes(x = year, y = value,
-                    group = variable, fill = variable)) +
-    stat_summary(fun.y="sum", geom="area", position="stack", col="black")
-    geom_line() 
-
-
+plot(r)
 
